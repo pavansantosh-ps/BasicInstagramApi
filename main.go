4b4e6a648f002329641d1a67ebf69179a25f7c36
pavansantosh-ps <pavansantosh.ips@gmail.com>
2021-10-09 10:48:07 +0530

    hashing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	helper "internship/instragram/helpers"
 	"internship/instragram/models"
 	"log"
 	"net/http"
@@ -55,6 +56,10 @@ func users(w http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
 		var u models.User
 		err := decoder.Decode(&u)
+		if err != nil {
+			panic(err)
+		}
+		u.Password, err = helper.HashPassword(u.Password)
 		if err != nil {
 			panic(err)
 		}
